0446d35e38c57603a4b429faea39288d59eada3f
aubustou <survivalfr@yahoo.fr>
2023-02-11 11:04:44 +0100

    Refacto pdf_to_cbz

--- range_bd/pdf_to_cbz.py
+++ range_bd/pdf_to_cbz.py
@@ -6,50 +6,75 @@ import subprocess
 import glob
 import send2trash
 
-BDS = [
-
-]
+BDS = []
 BD_FOLDER = Path(r"D:\Bédés")
 FILENAME_PATTERN = re.compile(r"(.*)_Page\.pdf-\d+\.(?:png|jpg)")
 MAX_PER_RUN = 10
+DPI = 300
+
+
+def compress(bd_path: Path) -> list[Path]:
+    bd_name = bd_path.stem
+
+    images: list[Path] = []
+    for img_path in bd_path.parent.glob(glob.escape(bd_name) + "_Page.pdf-*.png"):
+        images.append(img_path)
+
+    cbz_file_path = bd_path.with_suffix(".zip")
+    if not cbz_file_path.exists():
+        print(f"To CBZ: {cbz_file_path}")
+
+        with ZipFile(cbz_file_path, "w") as cbz_file:
+            for image in images:
+                cbz_file.write(str(image), image.name)
+    else:
+        print(f"CBZ already exists: {cbz_file_path}")
+
+    return images
+
+
+def convert_to_img(pdf_file: Path) -> Path:
+    print(f"To IMG: {pdf_file}")
+    subprocess.run(
+        [
+            "pdftoppm",
+            "-r",
+            str(DPI),
+            "-png",
+            str(pdf_file),
+            str(pdf_file.with_stem(pdf_file.stem + "_Page")),
+        ],
+        check=True,
+    )
+    return pdf_file
+
+
+def remove_images(images: list[Path]) -> None:
+    try:
+        send2trash.send2trash(images)
+    except FileNotFoundError:
+        pass
+
+
+def convert_from_list(bds: list[str]) -> None:
+    for bd in bds:
+        bd_path = Path(bd)
+        images = compress(bd_path)
+        remove_images(images)
+
 
 def main():
-    visited_paths: set[Path] = set()
-
     if BDS:
-        visited_paths = set(Path(path) for path in BDS)
-    else:
-        for index, pdf_file in enumerate(BD_FOLDER.rglob("*.pdf")):
-            print(f"To IMG: {pdf_file}")
-            subprocess.run(["pdftoppm", "-r", "300", "-png", str(pdf_file), str(pdf_file.with_stem(pdf_file.stem + "_Page"))], check=True)
-            visited_paths.add(pdf_file)
-            if index > MAX_PER_RUN:
-                break
+        return convert_from_list(BDS)
 
-    for bd_path in visited_paths:
+    for index, pdf_file in enumerate(BD_FOLDER.rglob("*.pdf")):
+        bd_path = convert_to_img(pdf_file)
+        images = compress(bd_path)
+        remove_images(images)
 
-        bd_name = bd_path.stem
+        if index > MAX_PER_RUN:
+            break
 
-        images: list[Path] = []
-        for img_path in bd_path.parent.glob(glob.escape(bd_name) + "_Page.pdf-*.png"):
-            images.append(img_path)
 
-        cbz_file_path = bd_path.with_suffix(".zip")
-        if not cbz_file_path.exists():
-            print(f"To CBZ: {cbz_file_path}")
-
-            with ZipFile(cbz_file_path, "w") as cbz_file:
-                for image in images:
-                    cbz_file.write(str(image), image.name)
-        else:
-            print(f"CBZ already exists: {cbz_file_path}")
-
-        for image in images:
-            try:
-                send2trash.send2trash(images)
-            except FileNotFoundError:
-                pass
-        
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
