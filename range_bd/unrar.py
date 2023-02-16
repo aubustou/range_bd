bb7e8ee80a85415185c215177696e9e111c7796b
aubustou <survivalfr@yahoo.fr>
2023-02-16 01:35:09 +0100

    Refactored unrar

--- range_bd/unrar.py
+++ range_bd/unrar.py
@@ -1,3 +1,5 @@
+import argparse
+import logging
 import subprocess
 import tempfile
 from pathlib import Path
@@ -6,32 +8,61 @@ from zipfile import ZipFile
 import send2trash
 
 BD_FOLDER = Path("D:/Bédés")
+RAR_TYPES = ["cbr", "rar"]
+
+
+def create_cbz(book: Path) -> None:
+    with tempfile.TemporaryDirectory() as tmp:
+        if (cbz_book := book.with_suffix(".zip")).exists():
+            logging.info("CBZ exists. Removing %s", book)
+            send2trash.send2trash(book)
+            return
+
+        logging.info("Extracting {book.name} to {tmp}")
+        try:
+            subprocess.run(
+                ["unrar", "e", str(book), str(tmp)],
+                check=True,
+                capture_output=True,
+            )
+        except subprocess.CalledProcessError as e:
+            if b"is not RAR archive" in e.stdout:
+                logging.warning("%s is not RAR archive", book)
+                return
+            else:
+                logging.warning(e.stdout)
+        else:
+            logging.info("Creating %s", cbz_book)
+
+            with ZipFile(cbz_book, "w") as zip_:
+                for file_ in Path(tmp).iterdir():
+                    zip_.write(file_)
+
+            logging.info("Removing %s", book)
+            send2trash.send2trash(book)
 
 
 def main():
-    for type_ in ["cbr", "rar"]:
-        for book in BD_FOLDER.rglob(f"*.{type_}"):
-            tmp = tempfile.mkdtemp()
-            print(f"{book.name} to {tmp}")
-            if (cbz_book := book.with_suffix(".zip")).exists():
-                send2trash.send2trash(book)
-                continue
-            try:
-                subprocess.run(["unrar", "e", str(book), str(tmp)], check=True,
-                               capture_output=True)
-            except subprocess.CalledProcessError as e:
-                if b"is not RAR archive" in e.stdout:
-                    print(f"{book} is not a RAR archive")
-                    continue
-                else:
-                    print(e.stdout)
-                    raise
-            else:
-                with ZipFile(cbz_book, "w") as zip_:
-                    for file_ in Path(tmp).iterdir():
-                        zip_.write(file_)
-                send2trash.send2trash(book)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "folders",
+        type=Path,
+        help="Folders containing the books",
+        default=BD_FOLDER,
+        nargs="?",
+    )
+
+    args = parser.parse_args()
+
+    folders = args.folders
+
+    logging.basicConfig(level=logging.INFO)
+
+    for type_ in RAR_TYPES:
+        for folder in folders:
+            for book in folder.rglob(f"*.{type_}"):
+                create_cbz(book)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
